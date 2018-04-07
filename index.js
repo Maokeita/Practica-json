2bef57533b6af1e4bcb221e9b55993df23b3b5ee
davidrodriguezlobo <35872962+davidrodriguezlobo@users.noreply.github.com>
2018-04-07 09:20:40 -0600

    actualizacion 3
    actualizacion 3

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ var Informacion = ("nombre":"Carlos");
 var Datainfo = JSON.parse(Informacion);
 console.log("El nombre "+Informacion.nombre);
 console.log("El nombre "+Informacion["nombre"]);
+
+var fs = required("fs");
+
+var Archivo = fs.readFileSync("data.json");
+var DataArchivo = JSON.parse(Archivo);
+Console.log("El correo es "+DataArchivo.correo);
